2d13561b6a4d069db61d8bbbf9f024be3ac9f528
Taha Kapasi <t.s.m.kapasi@gmail.com>
2020-12-07 11:01:41 +0530

    Removed comapny from main.py

--- main.py
+++ main.py
@@ -5,21 +5,3 @@ ANALYSIS = "/analysis"
 BALANCE_SHEET = "/balance-sheet"
 COMPANIES = []
 DATA_FILE = "processed_companies.pkl"
-
-
-class Company:
-    def __init__(self, **kwargs):
-        self.SYM = kwargs['SYM']
-        self.Name = kwargs['Name']
-        self.Price = kwargs['Price']
-        self.Volume = kwargs['Volume']
-        self.AverageVolume = kwargs['Average Volume']
-        self.PERatio = kwargs['P/E Ratio']
-        self.MarketCap = kwargs['Market Cap']
-        self.EPS = kwargs['EPS']
-        self.Industry = kwargs['Industry']
-        self.Sector = kwargs['Sector']
-        self.RevenueHistory = kwargs['Revenue History']
-        self.ProfitHistory = kwargs['Profit History']
-        self.InterestExpenseHistory = kwargs['Interest Expense History']
-        self.Liabilities = kwargs['Liabilities']
